41f156bf99cc4727b58b8828371e5e1dbfc6a7da
Anggito Muhammad Amien <109843500+anggito-m@users.noreply.github.com>
2026-02-24 00:29:23 +0700

    Fix turbopack configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  turbopack.root = __dirname,
+  turbopack: {
+    root: __dirname,
+  },
   allowedDevOrigins: [
     // all
     "*",
